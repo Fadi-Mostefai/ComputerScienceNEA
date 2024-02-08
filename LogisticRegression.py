7fd97e1f0c06f2a9dc757f9b18d6791be672531c
Fadi-Mostefai <fadimostefai@gmail.com>
2024-02-08 22:09:32 +0000

    Made the LogisticRegression.py better by implementing the use of L2 regularization.

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -1,11 +1,10 @@
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self, learning_rate=0.01, n_iters=1000, lambda_=0.1):
+    def __init__(self, learning_rate=0.01, n_iters=1000):
         self.lr = learning_rate
         self.n_iters = n_iters
-        self.lambda_ = lambda_  # Regularization parameter
-        self.weights = None
+        self.weights = None  # Consider initializing with zeros based on input feature size if necessary
         self.bias = None
 
     def fit(self, X, y):
@@ -13,13 +12,11 @@ class LogisticRegression:
         self.weights = np.zeros(n_features)
         self.bias = 0
 
-        # Gradient Descent with L2 Regularization
+        # Gradient Descent
         for _ in range(self.n_iters):
             model = np.dot(X, self.weights) + self.bias
             y_predicted = self._sigmoid(model)
-
-            # Regularization term added to the gradient
-            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y)) + (self.lambda_ / n_samples) * self.weights
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
             db = (1 / n_samples) * np.sum(y_predicted - y)
 
             self.weights -= self.lr * dw
